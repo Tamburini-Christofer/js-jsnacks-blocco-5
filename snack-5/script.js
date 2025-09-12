587142923f46dc4872c2e05339fccdb2029e6aa8
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-12 14:16:17 +0200

    Snack 5 compeltato

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,8 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
+let numeriPari = nums.filter(numero => numero % 2 === 0);
+
+console.log(`I numeri pari sono ${numeriPari}`)
+
 // Risultato: [2, 8, 4, 12]
