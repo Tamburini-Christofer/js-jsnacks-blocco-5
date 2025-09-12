059d4a88580e3e3dd0eb33937ae2b1c7ce5be9a0
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-12 13:55:21 +0200

    Snack 3 sistemato

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,7 +1,8 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
-const numberPlus = numbers.map(numero => numero++);
-console.log(numberPlus);
+const numberPlus = numbers.map(numero => numero + 1);
+console.log(`Array precedente: ${numbers}`);
+console.log(`Array aumentato: ${numberPlus}`);
 
 // Risultato: [3, 9, 5, 8, 3, 88] 
